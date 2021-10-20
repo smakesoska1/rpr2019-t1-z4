1349a8d630214d8a1e897c226fc975f847556e02
smakesoska1 <smakesoska1@etf.unsa.ba>
2021-10-20 17:00:43 +0200

    geteri i seteri u Artiklu

--- src/ba/unsa/etf/rpr/Artikl.java
+++ src/ba/unsa/etf/rpr/Artikl.java
@@ -1,4 +1,35 @@
 package ba.unsa.etf.rpr;
 
 public class Artikl {
+    private String naziv;
+    private int kod;
+    private String cijena;
+
+
+    public Artikl(String naziv, int kod, String cijena) {
+        this.naziv = naziv;
+        this.kod = kod;
+        this.cijena = cijena;
+    }
+
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public int getKod() {
+        return kod;
+    }
+
+    public String getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(String cijena) {
+        this.cijena = cijena;
+    }
 }
