e03d0b48c69ffbb4c09941ffb172588aaa056251
smakesoska1 <smakesoska1@etf.unsa.ba>
2021-10-20 17:03:18 +0200

    dodani prototipovi metoda

--- src/ba/unsa/etf/rpr/Korpa.java
+++ src/ba/unsa/etf/rpr/Korpa.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr;
 
 public class Korpa {
+    public Artikl[] getArtikli() {
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+    }
+
+    public boolean dodajArtikl(Artikl a) {
+    }
+
+    public int dajUkupnuCijenuArtikala() {
+    }
 }
--- src/ba/unsa/etf/rpr/Supermarket.java
+++ src/ba/unsa/etf/rpr/Supermarket.java
@@ -1,4 +1,12 @@
 package ba.unsa.etf.rpr;
 
 public class Supermarket {
+    public void dodajArtikl(Artikl biciklo) {
+    }
+
+    public Artikl izbaciArtiklSaKodom(String kod) {
+    }
+
+    public Artikl[] getArtikli() {
+    }
 }
